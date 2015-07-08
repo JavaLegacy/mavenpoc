f4e42299daabf911cdbf69371c4c47a08289a839
saulocamposi <saulocamposi@gmail.com>
2015-07-08 18:55:22 -0300

    Test commit using eclipse git plugin

--- pocjpa/src/main/java/br/com/pocjpa/config/StartJPA.java
+++ pocjpa/src/main/java/br/com/pocjpa/config/StartJPA.java
@@ -3,6 +3,11 @@ package br.com.pocjpa.config;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
+/**
+ * 
+ * @author s
+ *
+ */
 public class StartJPA {
 
 	public static void main(String[] args) {
